14ebb660637e53369ced6f36ccd9ce1540f548e4
Rishabh Paudel <24BCS10110@cuchd.in>
2026-04-25 19:17:52 +0530

    updated whole website

---
+++ script.js
@@ -0,0 +1,7 @@
+function download() {
+  window.open("https://github.com/noicOrg/notebook.me.main/releases/tag/Releases");
+}
+
+function github() {
+  window.open("https://github.com/noicOrg/notebook.me.main");
+}
