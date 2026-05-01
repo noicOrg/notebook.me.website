aa248afc004a280c876a048b9b68a00d2f457e2f
Rishabh Paudel <24BCS10110@cuchd.in>
2026-05-01 22:42:00 +0530

    added typing animation and smooth scroll

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function download() {
 function github() {
   window.open("https://github.com/noicOrg/notebook.me.main");
 }
+
+// Typing animation
+const text = "Trusted by 2+ people";
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typing").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 60);
+  }
+}
+
+window.onload = typeEffect;
